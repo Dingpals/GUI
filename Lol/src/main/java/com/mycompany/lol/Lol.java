d0f71c58552877c91c9444b76c1ca8e1a331e000
Dingpals <150411456+Dingpals@users.noreply.github.com>
2023-11-22 07:57:02 +0800

    Add files via upload

---
+++ Lol/src/main/java/com/mycompany/lol/Lol.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lol;
+
+/**
+ *
+ * @author USER
+ */
+public class Lol {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
